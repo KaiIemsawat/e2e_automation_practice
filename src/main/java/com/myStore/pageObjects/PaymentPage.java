0c7e688fb644f31258ffb6fbf6427caf57b4f502
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-02 17:38:35 -0400

    payment page method

--- src/main/java/com/myStore/pageObjects/PaymentPage.java
+++ src/main/java/com/myStore/pageObjects/PaymentPage.java
@@ -1,3 +1,24 @@
 package com.myStore.pageObjects;
 
-public class PaymentPage {}
+import com.myStore.actionDriver.Action;
+import com.myStore.base.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class PaymentPage extends BaseClass {
+  @FindBy(xpath = "//a[@title='Pay by bank wire']")
+  WebElement payByBankWireBtn;
+
+  @FindBy(xpath = "//a[@title='Pay by check.']")
+  WebElement payByCheckBtn;
+
+  public PaymentPage() {
+    PageFactory.initElements(driver, this);
+  }
+
+  public OrderSummaryPage clickOnBankWire() {
+    Action.click(driver, payByBankWireBtn);
+    return new OrderSummaryPage();
+  }
+}
