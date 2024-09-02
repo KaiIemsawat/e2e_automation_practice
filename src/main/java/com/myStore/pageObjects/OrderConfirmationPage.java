26e2352ca8042d8daa1e6ea14644411628ffb5c9
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-02 17:58:33 -0400

    wrap up all pages

--- src/main/java/com/myStore/pageObjects/OrderConfirmationPage.java
+++ src/main/java/com/myStore/pageObjects/OrderConfirmationPage.java
@@ -1,3 +1,20 @@
 package com.myStore.pageObjects;
 
-public class OrderConfirmationPage {}
+import com.myStore.actionDriver.Action;
+import com.myStore.base.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class OrderConfirmationPage extends BaseClass {
+  @FindBy(xpath = "//p[@class='alert alert-success']")
+  WebElement orderCompleteMessage;
+
+  public OrderConfirmationPage() {
+    PageFactory.initElements(driver, this);
+  }
+
+  public String validateConfirmMessage() {
+    return orderCompleteMessage.getText();
+  }
+}
--- src/main/java/com/myStore/pageObjects/OrderSummaryPage.java
+++ src/main/java/com/myStore/pageObjects/OrderSummaryPage.java
@@ -1,3 +1,21 @@
 package com.myStore.pageObjects;
 
-public class OrderSummaryPage {}
+import com.myStore.actionDriver.Action;
+import com.myStore.base.BaseClass;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class OrderSummaryPage extends BaseClass {
+  @FindBy(xpath = "//button[@type='submit' and @class='button btn btn-default button-medium']")
+  WebElement confirmOrderBtn;
+
+  public OrderSummaryPage() {
+    PageFactory.initElements(driver, this);
+  }
+
+  public OrderConfirmationPage clickOnConfirmOrder() {
+    Action.click(driver, confirmOrderBtn);
+    return new OrderConfirmationPage();
+  }
+}
